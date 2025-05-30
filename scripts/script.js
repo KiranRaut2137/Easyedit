b401e02d98927a84c71b4c7d2627b285612d78c1
KiranRaut2137 <rautkiran700@gmail.com>
2025-05-30 18:37:08 +0530

    setting pannel update and sum events added

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
+//all get elements
 
 let inputimg = document.getElementById("main-img")
-
 let imgcontainer = document.getElementById("edit-img")
-
 let editImg = document.getElementById("img-main")
-
 let imguploder = document.getElementById("img-uploder")
+let openpnnelsetting = document.getElementById("pannel-setting-open")
+let settinghandelere = document.getElementById("setting")
+let mainsettingpannel = document.getElementById("setting-pannel") 
+
+// Image upload events
 
 inputimg.addEventListener("change", () => {
     let imgFile = inputimg.files[0]
@@ -18,4 +21,18 @@ inputimg.addEventListener("change", () => {
 
     imguploder.style.display = "none"
     imgcontainer.style.display = "block"
+})
+
+// open setting pannel events
+
+openpnnelsetting.addEventListener("click",()=>{
+    settinghandelere.style.display = "block"
+})
+
+settinghandelere.addEventListener("click",()=>{
+    settinghandelere.style.display = "none"
+})
+
+mainsettingpannel.addEventListener("click",(e)=>{
+    e.stopPropagation()
 })
